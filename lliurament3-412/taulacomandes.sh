6adb1abc4a0d1e9e6cfb538aacedbe46204fa07b
Nat <NataliaCortesD@autonoma.cat>
2022-11-07 11:22:22 +0100

    Natàlia Cortés: Master C3

--- lliurament3-412/taulacomandes.sh
+++ lliurament3-412/taulacomandes.sh
@@ -9,3 +9,18 @@ git log
 gedit taula.txt
 git add .
 git commit -m "Natàlia Cortés: Master C2"
+git log --oneline --all
+git branch Branca1
+git checkout Branca1
+gedit taula.txt
+git add .
+git commit -m "Natàlia Cortés: Branca1 C1"
+git log --oneline --all
+gedit taula.txt
+git add . 
+git commit -m "Natàlia Cortés: Branca1 C2"
+git log -oneline -all
+git checkout master 
+gedit taula.txt
+git add .
+git commit -m "Natàlia Cortés: Master C3"
