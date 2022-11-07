5696496770907c377501d8b2d006aaaea4704b6d
Nat <NataliaCortesD@autonoma.cat>
2022-11-07 10:57:22 +0100

    Natàlia Cortés: Master C1

---
+++ lliurament3-412/taulacomandes.sh
@@ -0,0 +1,7 @@
+mkdir lliurament3-412
+cd lliurament3-412
+git init 
+git remote add lliurament3-412 https://github.com/NIU1668991/lliurament3--412.git
+gedit taula.txt
+git add .
+git commit -m "Natàlia Cortés: Master C1"
